8839d08f0e660583c5ea9d719519a1eaea68c52a
Seung Whan (Peter) Song <75101190+Pikklefish@users.noreply.github.com>
2024-05-09 14:30:25 -0400

    finsihed data cleaning

--- main.py
+++ main.py
@@ -4,8 +4,24 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from imblearn.over_sampling import RandomOverSampler
 
+# Bock,R.. (2007). MAGIC Gamma Telescope. UCI Machine Learning Repository. https://doi.org/10.24432/C52C8B.
+
+
+# Set up data coloumns titles (features) and read in csv file
 cols = ["fLength", "fWidth", "fSize", "fConc", "fConc1", "fAsym", "fM3Long", "fM3Trans", "fAlpha", "fDist", "class"]
 df = pd.read_csv("magic04.data", names=cols)
 df.head()
+# print(df.head())
 
-print(df.head())
+# Compares 'class' coloumn (label) with string "g" and returns boolean (true or false). astype(int) converts it to (true=1, false=0)
+df["class"]=(df["class"] == "g").astype(int)
+# print(df.head())
+
+for label in cols[:-1]:
+  plt.hist(df[df["class"]==1][label], color='blue', label='gamma', alpha=0.7, density=True)
+  plt.hist(df[df["class"]==0][label], color='red', label='hadron', alpha=0.7, density=True)
+  plt.title(label)
+  plt.ylabel("Probability")
+  plt.xlabel(label)
+  plt.legend()
+  plt.show()
