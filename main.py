c66f2ddcca180a2dc3d314bf15e55e00e0004d6e
Seung Whan (Peter) Song <75101190+Pikklefish@users.noreply.github.com>
2024-05-09 14:55:24 -0400

    fixed plot showing

--- main.py
+++ main.py
@@ -18,10 +18,14 @@ df["class"]=(df["class"] == "g").astype(int)
 # print(df.head())
 
 for label in cols[:-1]:
-  plt.hist(df[df["class"]==1][label], color='blue', label='gamma', alpha=0.7, density=True)
-  plt.hist(df[df["class"]==0][label], color='red', label='hadron', alpha=0.7, density=True)
-  plt.title(label)
-  plt.ylabel("Probability")
-  plt.xlabel(label)
-  plt.legend()
-  plt.show()
+   plt.figure()
+   plt.hist(df[df["class"]==1][label], color='blue', label='gamma', alpha=0.7, density=True)
+   plt.hist(df[df["class"]==0][label], color='red', label='hadron', alpha=0.7, density=True)
+   plt.title(label)
+   plt.ylabel("Probability")
+   plt.xlabel(label)
+   plt.legend()
+#    plt.savefig(f"{label}.png")  # Save the figure as a PNG
+#    plt.close()  # Close the figure to free memory
+plt.show()
+
