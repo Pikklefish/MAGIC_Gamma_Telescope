9319104eee895c5ad12b15aa72d9d29e8697eea8
Seung Whan (Peter) Song <75101190+Pikklefish@users.noreply.github.com>
2024-05-22 00:12:33 -0400

    starting naive bayes

--- main.py
+++ main.py
@@ -75,25 +75,26 @@ train = scale_dataset(train_df,selected_features)
 valid = scale_dataset(valid_df,selected_features)
 test = scale_dataset(test_df,selected_features)
 
-##### <<KNN Model>> #####
+# ##### <<KNN Model>> #####
 
-for k in range (1,11):
-    knn_model = KNeighborsClassifier(n_neighbors=k)
+# for k in range (1,11):
+#     knn_model = KNeighborsClassifier(n_neighbors=k)
 
-    #####Oversampled
-    knn_model.fit(features_train_oversample, label_train_oversample)
+#     #####Oversampled
+#     knn_model.fit(features_train_oversample, label_train_oversample)
 
-    label_pred_oversample_valid = knn_model.predict(features_valid)
-    print(f"Classification Report for Oversample k={k}:\n")
-    print(classification_report(label_valid,label_pred_oversample_valid))
+#     label_pred_oversample_valid = knn_model.predict(features_valid)
+#     print(f"Classification Report for Oversample k={k}:\n")
+#     print(classification_report(label_valid,label_pred_oversample_valid))
 
-    #####Undersampled
-    knn_model.fit(features_train_undersample, label_train_undersample)
+#     #####Undersampled
+#     knn_model.fit(features_train_undersample, label_train_undersample)
 
-    label_pred_undersample_valid = knn_model.predict(features_valid)
-    print(f"Classification Report for Undersample k={k}:\n")
-    print(classification_report(label_valid,label_pred_undersample_valid))
+#     label_pred_undersample_valid = knn_model.predict(features_valid)
+#     print(f"Classification Report for Undersample k={k}:\n")
+#     print(classification_report(label_valid,label_pred_undersample_valid))
 
 
+##### <<Naive Bayes Model>> #####
 
 #this is the feature selected branch branch
